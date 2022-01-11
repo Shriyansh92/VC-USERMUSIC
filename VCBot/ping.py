e3b1dd79094775cfcb60c036815c96ac09bb4614
$HRÎY∆ÑSH <95535644+Shriyansh92@users.noreply.github.com>
2022-01-11 11:53:12 +0530

    Add files via upload

---
+++ VCBot/ping.py
@@ -0,0 +1,52 @@
+import os
+import sys
+import asyncio
+from pyrogram import Client
+from pyrogram import filters
+from pyrogram.types import Message
+from config import bot, call_py, HNDLR, contact_filter
+from time import time
+from datetime import datetime
+
+# System Uptime
+START_TIME = datetime.utcnow()
+TIME_DURATION_UNITS = (
+    ('Week', 60 * 60 * 24 * 7),
+    ('Day', 60 * 60 * 24),
+    ('Hour', 60 * 60),
+    ('Min', 60),
+    ('Sec', 1)
+)
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return 'inf'
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append('{} {}{}'
+                         .format(amount, unit, "" if amount == 1 else "s"))
+    return ', '.join(parts)
+
+
+@Client.on_message(contact_filter & filters.command(['ping'], prefixes=f"{HNDLR}"))
+async def ping(client, m: Message):
+   start = time()
+   current_time = datetime.utcnow()
+   m_reply = await m.reply_text("`...`")
+   delta_ping = time() - start
+   uptime_sec = (current_time - START_TIME).total_seconds()
+   uptime = await _human_time_duration(int(uptime_sec))
+   await m_reply.edit(f"`{delta_ping 🏓Pong !! * 🕝Time taken: 1000:.3f} ms` \n**🛠️Service uptime:** - `{uptime}`")
+
+@Client.on_message(contact_filter & filters.command(['restart'], prefixes=f"{HNDLR}"))
+async def restart(client, m: Message):
+   await m.reply("`Restarting...`")
+   os.execl(sys.executable, sys.executable, *sys.argv)
+   # You probably don't need it but whatever
+   quit()
+
+@Client.on_message(contact_filter & filters.command(['help'], prefixes=f"{HNDLR}"))
+async def help(client, m: Message):
+   HELP = f"**🛠HELP MENU \n\nUSER ⚡ COMMANDS FOR EVERYONE \n{HNDLR}play \n{HNDLR}playfrom [channel] ; [n] - Plays last n songs from channel \n{HNDLR}playlist / {HNDLR}queue \n{HNDLR}ping to check bot status \n{HNDLR}help to see a list of commands \n⚡ COMMANDS FOR ADMINS \n{HNDLR}skip \n{HNDLR}pause and {HNDLR}resume \n{HNDLR}stop / {HNDLR}end  \n{HNDLR}restart"
+   await m.reply(HELP)
