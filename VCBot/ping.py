cda74dd38a2ec2aa7c83f0091713a894575b366d
$HRÎY∆ÑSH <95535644+Shriyansh92@users.noreply.github.com>
2022-01-11 13:01:53 +0530

    Update ping.py

--- VCBot/ping.py
+++ VCBot/ping.py
@@ -37,7 +37,8 @@ async def ping(client, m: Message):
    delta_ping = time() - start
    uptime_sec = (current_time - START_TIME).total_seconds()
    uptime = await _human_time_duration(int(uptime_sec))
-   await m_reply.edit(f"`{delta_ping Pong !! * 🕝Time taken: 1000:.3f} ms` \n**🛠️Service uptime:** - `{uptime}`")
+   await m_reply.edit(f"`{delta_ping * 1000:.3f} ms` \n**Standtime 🔥** - `{uptime}`")
+
 
 @Client.on_message(contact_filter & filters.command(['restart'], prefixes=f"{HNDLR}"))
 async def restart(client, m: Message):
