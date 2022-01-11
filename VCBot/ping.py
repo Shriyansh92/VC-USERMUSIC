48907f8734f53d2d19c7aa33178e484df9fac4e8
$HRÎY∆ÑSH <95535644+Shriyansh92@users.noreply.github.com>
2022-01-11 14:06:57 +0530

    Update ping.py

--- VCBot/ping.py
+++ VCBot/ping.py
@@ -1,12 +1,3 @@
-import os
-import sys
-import asyncio
-from pyrogram import Client
-from pyrogram import filters
-from pyrogram.types import Message
-from config import bot, call_py, HNDLR, contact_filter
-from time import time
-from datetime import datetime
 
 # System Uptime
 START_TIME = datetime.utcnow()
@@ -37,8 +28,7 @@ async def ping(client, m: Message):
    delta_ping = time() - start
    uptime_sec = (current_time - START_TIME).total_seconds()
    uptime = await _human_time_duration(int(uptime_sec))
-   await m_reply.edit(f"`{delta_ping * 1000:.3f} ms` \n**Standtime 🔥** - `{uptime}`")
-
+   await m_reply.edit(f"`{delta_ping * 1000:.3f} ms` \n**Uptime ⏳** - `{uptime}`")
 
 @Client.on_message(contact_filter & filters.command(['restart'], prefixes=f"{HNDLR}"))
 async def restart(client, m: Message):
@@ -49,5 +39,6 @@ async def restart(client, m: Message):
 
 @Client.on_message(contact_filter & filters.command(['help'], prefixes=f"{HNDLR}"))
 async def help(client, m: Message):
-   HELP = f" 🛠help menu \n\n⚡ COMMANDS FOR EVERYONE \n{HNDLR}play \n{HNDLR}playfrom [channel] ; [n] - Plays last n songs from channel \n{HNDLR}playlist / {HNDLR}queue \n{HNDLR}ping \n\n⚡ COMMANDS FOR ADMINS \n{HNDLR}skip \n{HNDLR}pause and {HNDLR}resume \n{HNDLR}stop / {HNDLR}end \n{HNDLR}restart"
+   HELP = f"**" 🛠help menu \n\n⚡ COMMANDS FOR EVERYONE \n{HNDLR}play \n{HNDLR}playfrom [channel] ; [n] - Plays last n songs from channel \n{HNDLR}playlist / {HNDLR}queue \n{HNDLR}ping \n\n⚡ COMMANDS FOR ADMINS \n{HNDLR}skip \n{HNDLR}pause and {HNDLR}resume \n{HNDLR}stop / {HNDLR}end \n{HNDLR}restart"
    await m.reply(HELP)
+
